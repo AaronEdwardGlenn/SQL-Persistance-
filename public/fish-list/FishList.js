ec6ab6bffe1df6edcb6250925f72fd8f23db2d23
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2019-11-07 11:33:25 -0800

    added FishList.js

---
+++ public/fish-list/FishList.js
@@ -0,0 +1,25 @@
+import Component from '../Component.js'; 
+import FishItem from './FishItem.js'; 
+
+class FishList extends Component {
+
+    onRender(dom) {
+        const fishes = this.props.fishes; 
+        fishes.forEach(fish => {
+            const props = { fish: fish }; 
+            const fishItem = new FishItem(props); 
+            const fishItemDOM = fishItem.renderDOM(); 
+            dom.appendChild(fishItemDOM); 
+
+        }); 
+    }
+
+
+    renderHTML() {
+        return /*html*/`
+        <ul class="fish"></ul>
+        `;
+    }
+}
+
+export default FishList; 
