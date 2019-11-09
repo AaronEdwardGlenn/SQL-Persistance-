00dcb257f7e8e9c7bcfa9ff933c43abd5765c8f7
Ben Beekman <bbeekman@gmail.com>
2019-11-08 20:38:18 -0800

    fish list CSS

--- public/fish-list/FishItem.js
+++ public/fish-list/FishItem.js
@@ -3,10 +3,9 @@ import Component from '../Component.js';
 class FishItem extends Component {
     renderHTML() {
         const fish = this.props.fish;
-        console.log(fish, 'xxxx'); 
         return /*html*/ `
         <li class='fish-item'>
-            <div class ="info-container">
+            <div class="info-container">
             <h2>${fish.name}</h2>
             <p class="fish-species">${fish.species}</p>
             </div>
@@ -15,10 +14,10 @@ class FishItem extends Component {
             <img src="${fish.url}" alt="${fish.name} image">
             </div>
 
-            <p class="typical_weight_oz">${fish.typicalweightoz}</p>
-            <p class="fresh_water">${fish.freshwater}</p>
-            <p class="salt_water">${fish.saltwater}</p>
-            <p class="zone">${fish.zone}</p>
+            <p class="typical_weight_oz">Typical Weight:${fish.typicalweightoz} oz.</p>
+            <p class="fresh_water">Fresh Water: ${fish.freshwater}</p>
+            <p class="salt_water">Salt Water: ${fish.saltwater}</p>
+            <p class="zone">Zone: ${fish.zone}</p>
             </li>
         `;
     }
