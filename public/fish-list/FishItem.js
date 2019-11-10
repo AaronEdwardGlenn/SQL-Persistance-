523cf9d61b5cd7e5d58d23bc28d759c1bafeee29
Ben Beekman <bbeekman@gmail.com>
2019-11-10 14:01:20 -0800

    switching to plain text badges

--- public/fish-list/FishItem.js
+++ public/fish-list/FishItem.js
@@ -4,6 +4,8 @@ class FishItem extends Component {
     renderHTML() {
         const fish = this.props.fish;
         const fresh_badge = (fish.freshwater === true) ? '../assets/fresh_badge.png' : '../assets/transparent.gif';
+        const fresh_text = (fish.freshwater === true) ? 'Fresh Water' : '';
+        const salt_text = (fish.saltwater === true) ? 'Salt Water' : '';
         const salt_badge = (fish.saltwater === true) ? '../assets/salt_badge.png' : '../assets/transparent.gif';
         return /*html*/ `
         <li class='fish-item'>
@@ -15,10 +17,9 @@ class FishItem extends Component {
             <div class="image-container">
             <img src="${fish.url}" alt="${fish.name} image">
             </div>
-
+            <span class="freshwater saltwater>${fresh_text} ${salt_text}</span>
             <p class="typical_weight_oz">Typical Weight:${fish.typicalweightoz} oz.</p>
-            <p class="fresh_water"><img src="${fresh_badge}"></p>
-            <p class="salt_water"><img src="${salt_badge}"></p>
+
             <p class="zone">Zone: ${fish.zone}</p>
             </li>
         `;
