f8fcf9a84f0447f0545808740d74c51c9fd739b1
Ben Beekman <bbeekman@gmail.com>
2019-11-10 13:43:56 -0800

    implementing saltwater and freshwater badges

--- public/fish-list/FishItem.js
+++ public/fish-list/FishItem.js
@@ -3,6 +3,8 @@ import Component from '../Component.js';
 class FishItem extends Component {
     renderHTML() {
         const fish = this.props.fish;
+        const fresh_badge = (fish.freshwater === true) ? '../assets/fresh_badge.png' : '../assets/transparent.gif';
+        const salt_badge = (fish.saltwater === true) ? '../assets/salt_badge.png' : '../assets/transparent.gif';
         return /*html*/ `
         <li class='fish-item'>
             <div class="info-container">
@@ -15,8 +17,8 @@ class FishItem extends Component {
             </div>
 
             <p class="typical_weight_oz">Typical Weight:${fish.typicalweightoz} oz.</p>
-            <p class="fresh_water">Fresh Water: ${fish.freshwater}</p>
-            <p class="salt_water">Salt Water: ${fish.saltwater}</p>
+            <p class="fresh_water"><img src="${fresh_badge}"></p>
+            <p class="salt_water"><img src="${salt_badge}"></p>
             <p class="zone">Zone: ${fish.zone}</p>
             </li>
         `;
