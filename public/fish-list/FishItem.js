e77445550742e4cfda79266f67c0c33e288e6a1c
Ben Beekman <bbeekman@gmail.com>
2019-11-11 09:23:51 -0800

    url fixes

--- public/fish-list/FishItem.js
+++ public/fish-list/FishItem.js
@@ -3,10 +3,8 @@ import Component from '../Component.js';
 class FishItem extends Component {
     renderHTML() {
         const fish = this.props.fish;
-        const fresh_badge = (fish.freshwater === true) ? '../assets/fresh_badge.png' : '../assets/transparent.gif';
         const fresh_text = (fish.freshwater === true) ? 'Fresh Water' : '';
         const salt_text = (fish.saltwater === true) ? 'Salt Water' : '';
-        const salt_badge = (fish.saltwater === true) ? '../assets/salt_badge.png' : '../assets/transparent.gif';
         return /*html*/ `
         <li class='fish-item'>
             <div class="info-container">
@@ -17,7 +15,7 @@ class FishItem extends Component {
             <div class="image-container">
             <img src="${fish.url}" alt="${fish.name} image">
             </div>
-            <span class="freshwater saltwater">${fresh_text} ${salt_text}</span>
+            <span class="badges">${fresh_text} ${salt_text}</span>
             <p class="typical_weight_oz">Typical Weight:${fish.typicalweightoz} oz.</p>
 
             <p class="zone">Zone: ${fish.zone}</p>
