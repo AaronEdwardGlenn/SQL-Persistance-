9c4ca3bf5084c3989cd654d2de7101c1b457290c
Ben Beekman <bbeekman@gmail.com>
2019-11-11 09:57:51 -0800

    navigation CSS improvements

--- public/common/Header.js
+++ public/common/Header.js
@@ -2,7 +2,7 @@ import Component from '../Component.js';
 
 class Header extends Component {
     renderHTML() {
-        return /*html*/ `<header><nav><ul><li><a href="../">Home</a></li><a href="../fish-list.html">Fish List</a><li><a href="../fish-form/">Fish Form</a></li></header>`;
+        return /*html*/ `<header><nav><ul><li><a href="../">Home</a></li><a href="../fish-list.html">Fish List</a><li><a href="../fish-form.html">Fish Form</a></li></header>`;
     }
 }
 
